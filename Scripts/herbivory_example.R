43db2a3d2640c215391187e01a722ee4efd33814
gwendul <gwlloyd@iu.edu>
2026-04-19 00:52:39 -0400

    updated herbivory models from CIEE workshop day 3

--- Scripts/herbivory_example.R
+++ Scripts/herbivory_example.R
@@ -10,7 +10,8 @@ source("R/derive_millennial_parms.R")
 source("R/init_millennial_state.R")
 source("R/plot_ode_out.R")
 
-parms <- yaml::read_yaml("config/common.yml")
+
+parms <- yaml::read_yaml("config/common_herbivore.yml")
 parms  <- modifyList(parms, yaml::read_yaml("config/millennial.yml"))
 parms  <- modifyList(parms, yaml::read_yaml("config/millennial_herbnem.yml"))
 
@@ -21,13 +22,42 @@ parms <- derive_millennial_parms(parms)
 
 parms$wood_mortality = 0
 
-# With detritivores:
+# With herbivores:
 millennial_herbnem = ode(
-  times = seq(1, 365*3, by = 1),
+  times = seq(1, 365*3000, by = 1),
   y     = init_millennial_state(HerbNem = T),
   func  = millennial_model_herbnem,
   parms = parms
 )
 
 plot_ode_output(millennial_herbnem,
+                variable_cols = names(init_millennial_state(HerbNem = T)))
+
+head(millennial_herbnem)
+tail(millennial_herbnem)
+
+
+# Stability Check (code from Janey)
+timeCheck_stable <- tibble(data.frame(millennial_herbnem)) %>%
+  filter(
+    time %in% c(
+      max(times),
+      max(times)-365,
+      max(times)-365*2
+    )
+  ) # this creates an empty tibble?
+
+timeEND <- as.data.frame(millennial_herbnem) %>%
+  slice_tail(n = 1) %>%
+  select(-time) %>%
+  unlist()
+
+new_herbnem <- ode(
+  y = timeEND,
+  times = times,
+  func = millennial_model_herbnem,
+  parms = parms
+)
+
+plot_ode_output(new_herbnem,
                 variable_cols = names(init_millennial_state(HerbNem = T)))
